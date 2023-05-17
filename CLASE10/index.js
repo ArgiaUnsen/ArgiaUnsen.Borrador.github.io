270f1cdb15689a886333cb2ff690b602b0ae69fe
Argia <argiaunsen372@gmail.com>
2023-05-17 20:40:38 -0300

    clase10

---
+++ CLASE10/index.js
@@ -0,0 +1,31 @@
+// variables - tipos
+var ejemplo; // no se usa
+let ejemplo1;
+const ejemplo3 = "Ejemplo";
+
+// variables - tipos de datos
+const edad = 21 // tipo number
+const nombre = "Argia" // tipo string
+const apellido = "unsen"; // tipo string
+let clases = ["clase0", "clase1", "clase2", "clase3"];
+let persona = {
+    nombre: "Argia",
+    apellido: "Unsen",
+    edad: 32,
+};
+let existePapaNoel = false;
+let programacion = true
+
+// log variables
+console.log("APRENDIENDO VARIABLES");
+console.log(edad);
+console.log(nombre);
+console.log(apellido);
+console.log(clases);
+console.log(persona);
+console.log(existePapaNoel);
+console.log(programacion);
+
+// nombrar variables
+
+let apelNom = "Pepito Juarez"
