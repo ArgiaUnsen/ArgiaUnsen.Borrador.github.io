bb6174a750e5d17d22324b37cf45329e48c33f48
Argia <argiaunsen372@gmail.com>
2023-05-25 10:11:46 -0300

    agrego ejemplos js

--- CLASE10/index.js
+++ CLASE10/index.js
@@ -29,3 +29,43 @@ console.log(programacion);
 // nombrar variables
 
 let apelNom = "Pepito Juarez"
+
+//switch case
+const palabra = "hola";
+switch (palabra) {
+  case "hola":
+    console.log("buenos dias");
+    break;
+  case "adios":
+    console.log("hasta pronto");
+    break;
+  default:
+    console.log("Hola mundo");
+    break;
+}
+
+//for
+const frutas = ["🍎", "🍏"];
+for (let index = 0; index < frutas.length; index++) {
+  console.log("El elemento es: ", frutas[index]);
+}
+
+//metodos de array: push
+const gente = ["MARCOS", "PRISCILA", "RAMIRO"];
+gente.push("NATALIA");
+console.log(gente);
+
+//metodos de array : pop
+const nombres = ["MARCOS", "PRISCILA", "RAMIRO"];
+nombres.push("NATALIA");
+console.log(nombres);
+nombres.pop();
+console.log(nombres);
+
+//while
+let count = 50;
+let contador = 0;
+while (count > contador) {
+  console.log("Hola chicos RD");
+  contador++;
+}
